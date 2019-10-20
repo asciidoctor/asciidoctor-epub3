c8ca2e297cd19e78fda0cdbd94d6b9fbca56c05c
Dan Allen <dan.j.allen@gmail.com>
2019-10-20 14:24:21 -0600

    collapse space before and after CDATA block

--- lib/asciidoctor-epub3/converter.rb
+++ lib/asciidoctor-epub3/converter.rb
@@ -152,8 +152,7 @@ class ContentConverter
 <title>#{doctitle_sanitized}</title>
 <link rel="stylesheet" type="text/css" href="styles/epub3.css"/>
 <link rel="stylesheet" type="text/css" href="styles/epub3-css3-only.css" media="(min-device-width: 0px)"/>
-#{icon_css_head}<script type="text/javascript">
-<![CDATA[
+#{icon_css_head}<script type="text/javascript"><![CDATA[
 document.addEventListener('DOMContentLoaded', function(event, reader) {
   if (!(reader = navigator.epubReadingSystem)) {
     if (navigator.userAgent.indexOf(' calibre/') >= 0) reader = { name: 'calibre-desktop' };
@@ -161,8 +160,7 @@ document.addEventListener('DOMContentLoaded', function(event, reader) {
   }
   document.body.setAttribute('class', reader.name.toLowerCase().replace(/ /g, '-'));
 });
-]]>
-</script>
+]]></script>
 </head>
 <body>
 <section class="chapter" title="#{doctitle_sanitized.gsub '"', '&quot;'}" epub:type="chapter" id="#{docid}">
