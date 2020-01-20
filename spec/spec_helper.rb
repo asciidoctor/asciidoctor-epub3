ce48362e0fa6a1165b6d08d173bedd517f9c51d0
Marat Radchenko <marat@slonopotamus.org>
2020-01-20 22:34:45 +0300

    Introduce helper functions for tests
    And rewrite tests using them.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,53 @@
 require 'asciidoctor-epub3'
 
 RSpec.configure do |config|
-  # configure rspec here
+  config.before do
+    FileUtils.rm_r temp_dir, force: true, secure: true
+  end
+
+  config.after do
+    FileUtils.rm_r temp_dir, force: true, secure: true
+  end
+
+  def bin_script name, opts = {}
+    path = Gem.bin_path (opts.fetch :gem, 'asciidoctor-epub3'), name
+    [Gem.ruby, path]
+  end
+
+  def asciidoctor_bin
+    bin_script 'asciidoctor', gem: 'asciidoctor'
+  end
+
+  def asciidoctor_epub3_bin
+    bin_script 'asciidoctor-epub3'
+  end
+
+  def run_command cmd, *args
+    Dir.chdir __dir__ do
+      if Array === cmd
+        args.unshift(*cmd)
+        cmd = args.shift
+      end
+      env_override = { 'RUBYOPT' => nil }
+      Open3.capture3 env_override, cmd, *args
+    end
+  end
+
+  def temp_dir
+    File.join __dir__, 'temp'
+  end
+
+  def temp_file path
+    File.join temp_dir, path
+  end
+
+  def examples_dir
+    File.join __dir__, '..', 'data', 'samples'
+  end
+
+  def example_file path
+    File.join examples_dir, path
+  end
 end
 
 RSpec::Matchers.define :have_size do |expected|
--- spec/xref_spec.rb
+++ spec/xref_spec.rb
@@ -1,103 +1,90 @@
 # frozen_string_literal: true
 
 require_relative 'spec_helper'
-require 'digest'
 require 'fileutils' unless defined? FileUtils
 
 describe 'Asciidoctor::Epub3::Converter - Xref' do
   context 'inter-chapter' do
-    it 'should resolve xref to top of chapter' do |example|
-      signature = Digest::SHA1.hexdigest example.full_description
-      book_dir = File.join __dir__, %(book-#{signature})
-      begin
-        Dir.mkdir book_dir
-        book_file = File.join book_dir, 'book.adoc'
-        chapter_a_file = File.join book_dir, 'chapter-a.adoc'
-        chapter_b_file = File.join book_dir, 'chapter-b.adoc'
+    it 'should resolve xref to top of chapter' do |_example|
+      FileUtils.mkdir_p temp_dir
+      book_file = File.join temp_dir, 'book.adoc'
+      chapter_a_file = File.join temp_dir, 'chapter-a.adoc'
+      chapter_b_file = File.join temp_dir, 'chapter-b.adoc'
 
-        File.write chapter_a_file, <<~'EOS', encoding: 'UTF-8'
-        = Chapter A
+      File.write chapter_a_file, <<~'EOS', encoding: 'UTF-8'
+      = Chapter A
 
-        This is chapter A.
-        There's not much too it.
+      This is chapter A.
+      There's not much too it.
 
-        Time to move on to <<chapter-b#>>.
-        EOS
+      Time to move on to <<chapter-b#>>.
+      EOS
 
-        File.write chapter_b_file, <<~'EOS', encoding: 'UTF-8'
-        = Chapter B
+      File.write chapter_b_file, <<~'EOS', encoding: 'UTF-8'
+      = Chapter B
 
-        Not much to show here either.
-        EOS
+      Not much to show here either.
+      EOS
 
-        File.write book_file, <<~'EOS', encoding: 'UTF-8'
-        = Book Title
-        :doctype: book
-        :idprefix:
-        :idseparator: -
+      File.write book_file, <<~'EOS', encoding: 'UTF-8'
+      = Book Title
+      :doctype: book
+      :idprefix:
+      :idseparator: -
 
-        include::chapter-a.adoc[]
+      include::chapter-a.adoc[]
 
-        include::chapter-b.adoc[]
-        EOS
+      include::chapter-b.adoc[]
+      EOS
 
-        doc = Asciidoctor.load_file book_file, backend: 'epub3', header_footer: true
-        doc.convert
-        spine_items = doc.references[:spine_items]
-        (expect spine_items).to have_size 2
-        chapter_a_content = doc.references[:spine_items][0].content
-        (expect chapter_a_content).to include '<a id="xref--chapter-b" href="chapter-b.xhtml" class="xref">Chapter B</a>'
-      ensure
-        FileUtils.rm_r book_dir, force: true, secure: true
-      end
+      doc = Asciidoctor.load_file book_file, backend: 'epub3', header_footer: true
+      doc.convert
+      spine_items = doc.references[:spine_items]
+      (expect spine_items).to have_size 2
+      chapter_a_content = doc.references[:spine_items][0].content
+      (expect chapter_a_content).to include '<a id="xref--chapter-b" href="chapter-b.xhtml" class="xref">Chapter B</a>'
     end
 
-    it 'should resolve xref to section inside chapter' do |example|
-      signature = Digest::SHA1.hexdigest example.full_description
-      book_dir = File.join __dir__, %(book-#{signature})
-      begin
-        Dir.mkdir book_dir
-        book_file = File.join book_dir, 'book.adoc'
-        chapter_a_file = File.join book_dir, 'chapter-a.adoc'
-        chapter_b_file = File.join book_dir, 'chapter-b.adoc'
+    it 'should resolve xref to section inside chapter' do |_example|
+      FileUtils.mkdir_p temp_dir
+      book_file = File.join temp_dir, 'book.adoc'
+      chapter_a_file = File.join temp_dir, 'chapter-a.adoc'
+      chapter_b_file = File.join temp_dir, 'chapter-b.adoc'
 
-        File.write chapter_a_file, <<~'EOS', encoding: 'UTF-8'
-        = Chapter A
+      File.write chapter_a_file, <<~'EOS', encoding: 'UTF-8'
+      = Chapter A
 
-        This is chapter A.
-        There's not much too it.
+      This is chapter A.
+      There's not much too it.
 
-        Time to move on to <<chapter-b#getting-started>>.
-        EOS
+      Time to move on to <<chapter-b#getting-started>>.
+      EOS
 
-        File.write chapter_b_file, <<~'EOS', encoding: 'UTF-8'
-        = Chapter B
+      File.write chapter_b_file, <<~'EOS', encoding: 'UTF-8'
+      = Chapter B
 
-        == Getting Started
+      == Getting Started
 
-        Now we can really get to it!
-        EOS
+      Now we can really get to it!
+      EOS
 
-        File.write book_file, <<~'EOS', encoding: 'UTF-8'
-        = Book Title
-        :doctype: book
-        :idprefix:
-        :idseparator: -
+      File.write book_file, <<~'EOS', encoding: 'UTF-8'
+      = Book Title
+      :doctype: book
+      :idprefix:
+      :idseparator: -
 
-        include::chapter-a.adoc[]
+      include::chapter-a.adoc[]
 
-        include::chapter-b.adoc[]
-        EOS
+      include::chapter-b.adoc[]
+      EOS
 
-        doc = Asciidoctor.load_file book_file, backend: 'epub3', header_footer: true
-        doc.convert
-        spine_items = doc.references[:spine_items]
-        (expect spine_items).to have_size 2
-        chapter_a_content = doc.references[:spine_items][0].content
-        (expect chapter_a_content).to include '<a id="xref--chapter-b--getting-started" href="chapter-b.xhtml#getting-started" class="xref">Getting Started</a>'
-      ensure
-        FileUtils.rm_r book_dir, force: true, secure: true
-      end
+      doc = Asciidoctor.load_file book_file, backend: 'epub3', header_footer: true
+      doc.convert
+      spine_items = doc.references[:spine_items]
+      (expect spine_items).to have_size 2
+      chapter_a_content = doc.references[:spine_items][0].content
+      (expect chapter_a_content).to include '<a id="xref--chapter-b--getting-started" href="chapter-b.xhtml#getting-started" class="xref">Getting Started</a>'
     end
   end
 end
