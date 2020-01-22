4c8db2e9da6a292fae74a3b28da65db94530a6ed
Marat Radchenko <marat@slonopotamus.org>
2020-01-22 17:09:14 +0300

    resolves #217 add support for Unicode characters in chapter IDs

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -28,5 +28,24 @@ describe Asciidoctor::Epub3::Converter do
           attributes: { 'ebook-format' => 'mobi' }
       expect(File).to exist(out_file)
     end
+
+    it 'converts chapter with unicode title to unicode filename' do
+      infile = fixture_file 'unicode/spine.adoc'
+      outfile = temp_file 'unicode.epub'
+      Asciidoctor.convert_file infile,
+          to_file: outfile,
+          backend: 'epub3',
+          header_footer: true,
+          mkdirs: true
+      prev_zip_encoding = Zip.force_entry_names_encoding
+      begin
+        Zip.force_entry_names_encoding = 'UTF-8'
+        Zip::File.open outfile do |zip|
+          expect(zip.find_entry('OEBPS/test-é.xhtml')).not_to be_nil
+        end
+      ensure
+        Zip.force_entry_names_encoding = prev_zip_encoding
+      end
+    end
   end
 end
