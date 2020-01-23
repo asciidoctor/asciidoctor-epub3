ead32bd138fa3b9c573b94d48d6f7a0af1dd8664
Marat Radchenko <marat@slonopotamus.org>
2020-01-23 12:20:50 +0300

    use consistent naming in tests

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -5,28 +5,28 @@ require_relative 'spec_helper'
 describe Asciidoctor::Epub3::Converter do
   describe '#convert' do
     it 'converts empty file to epub without exceptions' do
-      infile = fixture_file 'empty.adoc'
-      outfile = temp_file 'empty.epub'
-      Asciidoctor.convert_file infile,
-          to_file: outfile,
+      in_file = fixture_file 'empty.adoc'
+      out_file = temp_file 'empty.epub'
+      Asciidoctor.convert_file in_file,
+          to_file: out_file,
           backend: 'epub3',
           header_footer: true,
           mkdirs: true
-      expect(File).to exist(outfile)
+      expect(File).to exist(out_file)
     end
 
     it 'converts empty file to mobi without exceptions' do
       skip_if_darwin
 
-      infile = fixture_file 'empty.adoc'
-      outfile = temp_file 'empty.mobi'
-      Asciidoctor.convert_file infile,
-          to_file: outfile,
+      in_file = fixture_file 'empty.adoc'
+      out_file = temp_file 'empty.mobi'
+      Asciidoctor.convert_file in_file,
+          to_file: out_file,
           backend: 'epub3',
           header_footer: true,
           mkdirs: true,
           attributes: { 'ebook-format' => 'mobi' }
-      expect(File).to exist(outfile)
+      expect(File).to exist(out_file)
     end
   end
 end
