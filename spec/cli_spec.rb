057121a4a8c05f04493bb688f1a215be6e5895ae
Marat Radchenko <marat@slonopotamus.org>
2020-01-23 12:16:49 +0300

    introduce to_epub/to_mobi helper functions in cli_spec.rb (PR #247)

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -10,40 +10,35 @@ describe 'asciidoctor-epub3' do
   end
 
   it 'exits with 1 when given nonexistent path' do
-    _, err, res = run_command asciidoctor_epub3_bin, '/nonexistent'
+    _, err, res = to_epub '/nonexistent'
     expect(res.exitstatus).to eq(1)
     expect(err).to match(/input file \/nonexistent( is)? missing/)
   end
 
   it 'converts sample book to epub and validates it' do
-    infile = example_file 'sample-book.adoc'
-    outfile = temp_file 'sample-book.epub'
+    in_file = example_file 'sample-book.adoc'
+    out_file = temp_file 'sample-book.epub'
 
-    _, err, res = run_command asciidoctor_epub3_bin, '-a', 'ebook-validate', infile, '-o', outfile
+    _, err, res = to_epub in_file, out_file
     expect(res.exitstatus).to eq(0)
     expect(err).not_to include 'ERROR'
     expect(err).not_to include 'invalid reference'
-    expect(File).to exist(outfile)
+    expect(File).to exist(out_file)
   end
 
   it 'converts sample book to mobi' do
-    skip_if_darwin
+    in_file = example_file 'sample-book.adoc'
+    out_file = temp_file 'sample-book.mobi'
 
-    infile = example_file 'sample-book.adoc'
-    outfile = temp_file 'sample-book.mobi'
-
-    _, err, res = run_command asciidoctor_epub3_bin, '-a', 'ebook-format=mobi', infile, '-o', outfile
+    _, err, res = to_mobi in_file, out_file
     expect(res.exitstatus).to eq(0)
     expect(err).not_to include 'ERROR'
     expect(err).not_to include 'invalid reference'
-    expect(File).to exist(outfile)
+    expect(File).to exist(out_file)
   end
 
   it 'prints errors to stderr when converts invalid book to epub' do
-    infile = fixture_file 'empty.adoc'
-    outfile = temp_file 'empty.epub'
-
-    _, err, res = run_command asciidoctor_epub3_bin, '-a', 'ebook-validate', infile, '-o', outfile
+    _, err, res = to_epub fixture_file('empty.adoc'), temp_file('empty.epub')
     expect(res.exitstatus).to eq(0)
     # Error from epubcheck
     expect(err).to include 'ERROR(RSC-005)'
@@ -52,13 +47,19 @@ describe 'asciidoctor-epub3' do
   end
 
   it 'prints errors to stderr when converts invalid book to mobi' do
-    skip_if_darwin
-
-    infile = fixture_file 'empty.adoc'
-    outfile = temp_file 'empty.mobi'
-
-    _, err, res = run_command asciidoctor_epub3_bin, '-a', 'ebook-format=mobi', infile, '-o', outfile
+    _, err, res = to_mobi fixture_file('empty.adoc'), temp_file('empty.mobi')
     expect(err).to include 'ERROR'
     expect(res.exitstatus).to eq(0)
+  end
+
+  def to_mobi in_file, out_file
+    skip_if_darwin
+    run_command asciidoctor_epub3_bin, '-a', 'ebook-format=mobi', in_file, '-o', out_file
+  end
+
+  def to_epub in_file, out_file = nil
+    argv = asciidoctor_epub3_bin + ['-a', 'ebook-validate', in_file]
+    argv += ['-o', out_file] unless out_file.nil?
+    run_command argv
   end
 end
