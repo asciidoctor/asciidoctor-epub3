asciidoctor/asciidoctor-epub3

commit 87b135d61aa12d8706b9b00fcaca75d6bda36a88

resolves #332 avoid outputting 'true' for unsupported blocks

lib/asciidoctor-epub3/converter.rb
@@ -105,7 +105,8 @@ module Asciidoctor
         if respond_to? method_name
           send method_name, node
         else
-          logger.warn %(conversion missing in backend #{@backend} for #{name})
+          logger.warn %(#{::File.basename node.attr('docfile')}: conversion missing in backend #{@backend} for #{name})
+          nil
         end
       end
 

commit 6b27daa88517d19fa7fc0d664e5c61cc0fdb0cea

resolves #360 fix crash when encountering unsupported Asciidoctor node
Broken by 87b135d61aa12d8706b9b00fcaca75d6bda36a88

lib/asciidoctor-epub3/converter.rb
@@ -105,7 +105,7 @@ module Asciidoctor
         if respond_to? method_name
           send method_name, node
         else
-          logger.warn %(#{::File.basename node.attr('docfile')}: conversion missing in backend #{@backend} for #{name})
+          logger.warn %(conversion missing in backend #{@backend} for #{name})
           nil
         end
       end

spec/converter_spec.rb
@@ -262,6 +262,14 @@ Text
       expect(toc.href).to eq('toc.xhtml')
     end
 
+    it "doesn't crash when sees inline toc" do
+      to_epub <<~EOS
+= Title
+
+toc::[]
+      EOS
+    end
+
     it 'supports video' do
       book, = to_epub fixture_file('video/book.adoc')
       chapter = book.item_by_href '_chapter.xhtml'