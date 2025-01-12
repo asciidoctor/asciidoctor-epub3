7a883c060536daca98729e062e10a550abcf239f
Marat Radchenko <marat@slonopotamus.org>
2025-01-12 18:17:38 +0300

    reformat code

--- spec/templates_spec.rb
+++ spec/templates_spec.rb
@@ -16,10 +16,11 @@ EOS
 
   it 'supports ERB templates' do
     book, = to_epub <<~EOS, template_dirs: [fixtures_dir.join('templates/erb').to_s]
-= Article
+      = Article
 
-Paragraph
-EOS
+      Paragraph
+    EOS
+
     chapter = book.item_by_href '_article.xhtml'
     expect(chapter).not_to be_nil
     expect(chapter.content).to include '<erb>Paragraph</erb>'
